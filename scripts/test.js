2e1340cad244d5a31c0ec027f03d57e654a92cea
js23305 <jasminesingla652@gmail.com>
2024-10-28 05:03:02 +0530

    First version

---
+++ scripts/test.js
@@ -0,0 +1,26 @@
+var banana = document.getElementById("banana");
+banana.addEventListener("click", updateText)
+
+//Function to display the word Hello on the page
+function sayHello() {
+    alert("Hello")
+}
+
+// Function to change the content of pageHeader
+function updateText() {
+    const pageHeader = document.getElementById("pageHeader");
+    pageHeader.innerText = "BANANA!";
+}
+
+
+
+
+
+////////////////////////////
+
+// Hospital Database
+
+// patients {
+//
+//
+//}
